2fb3c59d4e80686b03692032f03fc6b70d1ca7a8
RAINBOW-SUBDONG\Rainbow <shuzichao@126.com>
2016-11-07 16:16:21 +0800

    [add]　活动发布 活动查询

--- src/main/java/com/zyx/rpc/activity/ActivityFacade.java
+++ src/main/java/com/zyx/rpc/activity/ActivityFacade.java
@@ -1,7 +1,7 @@
 package com.zyx.rpc.activity;
 
-//import com.zyx.param.activity.QueryActivityParm;
-//import com.zyx.param.activity.QueryHistoryParm;
+import com.zyx.param.activity.ActivityParam;
+import com.zyx.param.activity.QueryActivityParam;
 
 import java.util.Map;
 
@@ -15,59 +15,16 @@ import java.util.Map;
  */
 public interface ActivityFacade {
     /**
-     * @param createId
-     * @param title
-     * @param desc
-     * @param image
-     * @param startTime
-     * @param endTime
-     * @param lastTime
-     * @param maxPeople
-     * @param visible
-     * @param phone
-     * @param price
-     * @param type
-     * @param address
-     * @param examine
-     * @param memberTemplate
+     * 添加活动
+     * @param activityParam
      * @return
      */
-    Map<String, Object> insertActivity(Integer createId, String title, String desc, String image, Long startTime,
-                                       Long endTime, Long lastTime, Integer maxPeople, Integer visible,
-                                       String phone, Double price, Integer type, String address, Integer examine,
-                                       String memberTemplate, Long groupId);
+    Map<String, Object> insertActivity(ActivityParam activityParam);
 
     /**
-     * 多条条件查询活动
-     *
-     * @param parm
+     * 查询活动
+     * @param queryParam
      * @return
      */
-//    Map<String, Object> queryActivity(QueryActivityParm parm, int api);
-
-    /**
-     * 查询活动发起者自定义报名模板
-     *
-     * @param id
-     * @return
-     */
-    Map<String, Object> queryActivityMember(Integer id);
-
-    /**
-     * 组合名称查询
-     *
-     * @param name
-     * @return
-     */
-    Map<String, Object> queryActivityGroupName(String name);
-
-
-    /**
-     * 1  查询历史活动
-     * 2  时间范围查询活动
-     *
-     * @param history
-     * @return
-     */
-//    Map<String, Object> queryActivityHistory(QueryHistoryParm history);
+    Map<String, Object> queryActivity(QueryActivityParam queryParam);
 }
