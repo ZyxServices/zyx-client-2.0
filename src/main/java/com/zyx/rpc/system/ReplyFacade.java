eb4f2aae1e943dc925cf7d6a92da5f923412f6eb
XiaoWei <xiaoweiqb@126.com>
2016-11-11 18:33:50 +0800

    [add] 发布动态，动态详情，评论点赞

--- src/main/java/com/zyx/rpc/system/ReplyFacade.java
+++ src/main/java/com/zyx/rpc/system/ReplyFacade.java
@@ -9,5 +9,5 @@ import java.util.Map;
  * Create by XiaoWei on 2016/8/31
  */
 public interface ReplyFacade {
-    Map<String, Object> addReply(Integer replyParentId, Integer replyFromUser, Integer replyToUser, String replyContent);
+    Map<String, Object> addReply(Integer replyParentId, Integer replyFromUser, Integer replyToUser, String replyContent,String replyImgPath);
 }
