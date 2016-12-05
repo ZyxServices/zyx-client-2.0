6a9d7b7335a9c23caf370581c6fe44ee9b4ffc97
RAINBOW-SUBDONG\Rainbow <shuzichao@126.com>
2016-12-05 16:58:10 +0800

    [fix] 搜索模块

--- src/main/java/com/zyx/controller/system/SearchController.java
+++ src/main/java/com/zyx/controller/system/SearchController.java
@@ -1,7 +1,7 @@
 package com.zyx.controller.system;
 
-//import com.zyx.param.system.SearchParam;
 
+import com.zyx.constants.Constants;
 import com.zyx.param.system.SearchDevaParam;
 import com.zyx.param.system.SearchParam;
 import com.zyx.rpc.system.SearchFacade;
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.view.AbstractView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -33,9 +34,9 @@ public class SearchController {
     @RequestMapping(value = "/model", method = {RequestMethod.POST})
     @ApiOperation(value = "搜索查询", notes = "搜索查询")
     public ModelAndView model(@ApiParam(name = "character", value = "查询关键词") @RequestParam(name = "character", required = false) String character,
-                             @ApiParam(required = true, name = "model", value = "查询分类model（1、活动，2、装备，3、动态，4、场馆，5、教程，6、用户）") @RequestParam(name = "model") Integer model,
-                             @ApiParam(required = true, name = "number", value = "每页显示多少条") @RequestParam(name = "number") Integer number,
-                             @ApiParam(required = true, name = "pageNumber", value = "当前第几页") @RequestParam(name = "pageNumber") Integer pageNumber) {
+                              @ApiParam(required = true, name = "model", value = "查询分类model（1、活动，2、装备，3、动态，4、场馆，5、教程，6、用户）") @RequestParam(name = "model") Integer model,
+                              @ApiParam(required = true, name = "number", value = "每页显示多少条") @RequestParam(name = "number") Integer number,
+                              @ApiParam(required = true, name = "pageNumber", value = "当前第几页") @RequestParam(name = "pageNumber") Integer pageNumber) {
 
         AbstractView jsonView = new MappingJackson2JsonView();
         SearchParam searchParam = new SearchParam();
@@ -58,5 +59,59 @@ public class SearchController {
         Map<String, Object> map = searchFacade.devaSearch(searchDevaParam);
         jsonView.setAttributesMap(map);
         return new ModelAndView(jsonView);
+    }
+
+    @RequestMapping(value = "/modelAll", method = {RequestMethod.POST})
+    @ApiOperation(value = "搜索查询", notes = "搜索查询")
+    public ModelAndView modelAll(@ApiParam(name = "character", value = "查询关键词") @RequestParam(name = "character", required = false) String character,
+                                 @ApiParam(required = true, name = "number", value = "每页显示多少条") @RequestParam(name = "number") Integer number,
+                                 @ApiParam(required = true, name = "pageNumber", value = "当前第几页") @RequestParam(name = "pageNumber") Integer pageNumber) {
+
+        AbstractView jsonView = new MappingJackson2JsonView();
+        Map<String, Object> mapAll = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
+        getParam(character, number, pageNumber, 1);
+
+
+        Map<String, Object> mapModel1 = searchFacade.modularSearch(getParam(character, number, pageNumber, 1));
+        map.put("Activity", getMapData(mapModel1));
+
+        Map<String, Object> mapModel2 = searchFacade.modularSearch(getParam(character, number, pageNumber, 2));
+        map.put("Equip", getMapData(mapModel2));
+
+        Map<String, Object> mapModel3 = searchFacade.modularSearch(getParam(character, number, pageNumber, 3));
+        map.put("Concern", getMapData(mapModel3));
+
+        Map<String, Object> mapModel4 = searchFacade.modularSearch(getParam(character, number, pageNumber, 4));
+        map.put("Venues", getMapData(mapModel4));
+
+        Map<String, Object> mapModel5 = searchFacade.modularSearch(getParam(character, number, pageNumber, 5));
+        map.put("Course", getMapData(mapModel5));
+
+        Map<String, Object> mapModel6 = searchFacade.modularSearch(getParam(character, number, pageNumber, 6));
+        map.put("User", getMapData(mapModel6));
+
+        mapAll.put(Constants.STATE, Constants.SUCCESS);
+        mapAll.put(Constants.MSG_SUCCESS, Constants.MSG_SUCCESS);
+        mapAll.put(Constants.DATA,map);
+        jsonView.setAttributesMap(mapAll);
+        return new ModelAndView(jsonView);
+    }
+
+    private Object getMapData(Map<String, Object> map) {
+        if (map != null && map.get(Constants.STATE).equals(200)) {
+            return map.get(Constants.DATA);
+        } else {
+            return null;
+        }
+    }
+
+    private SearchParam getParam(String character, Integer number, Integer pageNumber, Integer model) {
+        SearchParam searchParam = new SearchParam();
+        searchParam.setCharacter(character);
+        searchParam.setModel(model);
+        searchParam.setPageNumber(pageNumber);
+        searchParam.setNumber(number);
+        return searchParam;
     }
 }
